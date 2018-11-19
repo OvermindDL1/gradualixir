ae461e809059a05482deaa753fb9fd09fa0ad8b6
OvermindDL1 <overminddl1@gmail.com>
2018-11-19 10:06:58 -0700

    Added ability to pass in file paths with wildcards for expanding as well as support umbrellas thus closing #1

--- lib/mix/tasks/gradualizer.ex
+++ lib/mix/tasks/gradualizer.ex
@@ -9,7 +9,8 @@ defmodule Mix.Tasks.Gradualizer do
   @command_options [
     gradualize_preload: :boolean,
     no_compile: :boolean,
-    quiet: :boolean
+    quiet: :boolean,
+    verbose: :boolean
   ]
 
   @spec run(OptionParser.argv()) :: :ok | :error
@@ -32,32 +33,75 @@ defmodule Mix.Tasks.Gradualizer do
     result
   end
 
-  defp run_gradualizer(opts, gargs) do
-    files =
-      case gargs do
-        [":all"] ->
-          :all
+  defp get_beams(type)
+  defp get_beams([":all"]), do: :all
+  defp get_beams([":project-ebin"]), do: get_beams([])
 
-        v when v in [[":project-ebin"], []] ->
-          Path.wildcard(Path.join([Mix.Project.compile_path(), "*.beam"]))
+  defp get_beams([]) do
+    Mix.Project.umbrella?()
+    |> if do
+      Mix.Project.apps_paths()
+      |> Map.keys()
+      |> Enum.map(fn app ->
+        [Mix.Project.build_path(), "lib", to_string(app), "ebin", "*.beam"]
+        |> Path.join()
+        |> Path.wildcard()
+      end)
+    else
+      [Mix.Project.compile_path(), "*.beam"]
+      |> Path.join()
+      |> Path.wildcard()
+    end
+    |> List.flatten()
+  end
 
-        [":project"] ->
-          Path.wildcard(Path.join([Mix.Project.compile_path(), "..", "*", "*.beam"]))
+  defp get_beams([":project"]) do
+    Mix.Project.umbrella?()
+    |> if do
+      Mix.Project.apps_paths()
+      |> Map.keys()
+      |> Enum.map(fn app ->
+        [Mix.Project.build_path(), "lib", to_string(app), "consolidated", "*.beam"]
+        |> Path.join()
+        |> Path.wildcard()
+      end)
+    else
+      [Mix.Project.compile_path(), "..", "consolidated", "*.beam"]
+      |> Path.join()
+      |> Path.wildcard()
+    end
+    |> List.flatten(get_beams([]))
+  end
 
-        [":deps"] ->
-          Path.wildcard(
-            Path.join([
-              Mix.Project.build_path(),
-              "*",
-              "*",
-              "*",
-              "*.beam"
-            ])
-          )
+  defp get_beams([":deps"]) do
+    [Mix.Project.build_path(), "**", "*.beam"]
+    |> Path.join()
+    |> Path.wildcard()
+  end
 
-        files ->
-          files
+  defp get_beams(paths) do
+    paths
+    |> Enum.map(&Path.expand/1)
+    |> Enum.flat_map(fn path ->
+      cond do
+        String.ends_with?(path, ".beam") ->
+          Path.wildcard(path)
+
+        path ->
+          [path, "**", "*.beam"]
+          |> Path.join()
+          |> Path.wildcard()
       end
+    end)
+  end
+
+  defp run_gradualizer(opts, gargs) do
+    files = get_beams(gargs)
+
+    opts[:verbose] && IO.puts("\n\nBeam Files to parse:")
+    opts[:verbose] && Enum.each(files, &IO.puts/1)
+
+    opts[:verbose] && IO.puts("\n\nGradualizing:")
 
     args = [
       preload: (opts[:gradualizer_preload] && true) || false
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gradualixir.MixProject do
   def project do
     [
       app: :gradualixir,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
